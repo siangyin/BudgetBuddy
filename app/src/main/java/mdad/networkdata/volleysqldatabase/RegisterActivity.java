b9ba77f6bfceecca2744d411db096e2f73d7e31d
SiangYin <siangyin916@gmail.com>
2023-07-15 07:10:15 +0800

    Login and Register action complete, directing intent inprogress

--- app/src/main/java/mdad/networkdata/volleysqldatabase/RegisterActivity.java
+++ app/src/main/java/mdad/networkdata/volleysqldatabase/RegisterActivity.java
@@ -20,66 +20,63 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
-    public static String apiAdd = "http://budgetbuddy.atspace.cc/api/v1";
-    // login url
-    private static String url_user_login =apiAdd+"/login.php";
-    Button loginBtn;
-    TextView tvRegister;
-    EditText etEmail, etPassword;
+public class RegisterActivity extends AppCompatActivity {
+    // register url
+    private static String url_user_register =MainActivity.apiAdd+"/register.php";
+
+    Button registerBtn;
+    TextView tvLogin;
+    EditText etName,etEmail,etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
 
         // Binding
-        loginBtn = findViewById(R.id.registerBtn);
-        tvRegister=findViewById(R.id.tvLogin);
-        etEmail=findViewById(R.id.etEmail);
-        etPassword=findViewById(R.id.etPassword);
+        registerBtn = findViewById(R.id.registerBtn);
+        tvLogin = findViewById(R.id.tvLogin);
+        etName = findViewById(R.id.etName);
+        etEmail = findViewById(R.id.etEmail);
+        etPassword = findViewById(R.id.etPassword);
 
-        // register link click event
-        tvRegister.setOnClickListener(new View.OnClickListener() {
+        // login link click event
+        tvLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent registerView = new Intent(getApplicationContext(), RegisterActivity.class);
-                startActivity(registerView);
+                Intent loginView = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(loginView);
             }
         });
 
-        // loginBtn click event
-        loginBtn.setOnClickListener(new View.OnClickListener() {
+        // registerBtn click event
+        registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String name = etName.getText().toString();
                 String email = etEmail.getText().toString();
                 String password = etPassword.getText().toString();
 
-                if(email.isEmpty() || password.isEmpty()){
+                if(name.isEmpty() || email.isEmpty() || password.isEmpty()){
                     Toast.makeText(getApplicationContext(), "Required fields cannot be empty", Toast.LENGTH_LONG).show();
                     return;
                 }
 
                 JSONObject reqBodyData = new JSONObject();
                 try {
+                    reqBodyData.put("name",name);
                     reqBodyData.put("email",email);
                     reqBodyData.put("password",password);
-
                 } catch (JSONException e) {
-
                 }
 
-                 // System.out.println(reqBodyData);
-                postData(url_user_login,reqBodyData);
-
+                System.out.println(reqBodyData);
+                postData(url_user_register,reqBodyData);
             }
-        });
-
-
-
+        }); // registerBtn event end
     } // onCreate End
 
-    public void postData(String url, final JSONObject json){
+    public void postData(String url,final JSONObject json){
         // create a RequestQueue for Volley
         RequestQueue reqQ = Volley.newRequestQueue(this);
 
@@ -112,5 +109,4 @@ public class MainActivity extends AppCompatActivity {
         reqQ.add(json_obj_req);
     }
 
-
-} // MainActivity End
+}
